392841e5661cc5c120761a7edc56271573f1ec55
Dylan Bowman <dylanbowmandsk@gmail.com>
2025-04-23 01:47:07 +0100

    added navbar and main splash

---
+++ src/app/splash.jsx
@@ -0,0 +1,12 @@
+const Splash = () => {
+    return(
+<div className="flex items-center justify-center min-h-screen bg-stone-950">
+  <div className="">
+    <h1 className="poppins-bold text-5xl text-zinc-100">Hello. I am <span className="text-violet-400">Dylan Bowman</span></h1>
+    <h1 className="poppins-regular text-4xl text-zinc-100">Developer and blockchain enthusiast</h1>
+  </div>
+</div>
+    )
+}
+
+export default Splash
