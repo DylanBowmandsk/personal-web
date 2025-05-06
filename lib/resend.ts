5a18dd0cf86b90b17ba2f7338ff99ecfeb5c8b7a
Dylan Bowman <dylanbowmandsk@gmail.com>
2025-05-06 20:34:51 +0100

    added changes to email handling

--- lib/resend.ts
+++ lib/resend.ts
@@ -4,11 +4,11 @@ import { Resend} from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export const sendEmail = async () => {
+export const sendEmail = async (name: string,email: string,message: string) => {
     resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: 'dylanbowmandsk@gmail.com',
-        subject: 'Hello World',
-        html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+        from: "onboarding@resend.dev",
+        to: "dylanbowmandsk@gmail.com",
+        subject: "New mail from portfolio website",
+        html: `<p> ${name}</p><p>${email}</p><p>${message}</p>`
       });
 }
--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react"
 import { sendEmail } from "../../lib/resend"
 
 const Contact = () => {
 
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+
     const send = () => {
-        sendEmail()
+        sendEmail(name,email,message)
     }
 
     return(
@@ -11,9 +16,9 @@ const Contact = () => {
             <h1 className="text-4xl text-violet-400 font-light poppins-light mb-10" data-aos="fade-right">{"Contact me"}</h1>
             <form action={send}>
                 <div className="flex flex-col">
-                    <input type="text" name="name" id="name" placeholder="name" className="border-b-1 h-14 w-140 text-xl border-solid mb-4"/>
-                    <input type="text" name="mail" id="email" placeholder="email" className="border-b-1  h-14 text-xl border-solid mb-4"/>
-                    <input type="text" name="message" id="message" placeholder="your message" className="border-b-1 h-14 text-xl border-solid mb-4"/>
+                    <input type="text" name="name" id="name" onChange={e => setName(e.target.value)} placeholder="name" className="border-b-1 h-14 w-140 text-xl border-solid mb-4"/>
+                    <input type="text" name="mail" id="email" onChange={e => setEmail(e.target.value)} placeholder="email" className="border-b-1  h-14 text-xl border-solid mb-4"/>
+                    <input type="text" name="message" id="message" onChange={e => setMessage(e.target.value)} placeholder="your message" className="border-b-1 h-14 text-xl border-solid mb-4"/>
                 </div>
                 <button type="submit" className="rounded-full bg-violet-400 hover:bg-violet-500 text-white poppins-semibold py-2 px-4 rounded-lg shadow-md transition-colors duration-200">Send</button>
             </form>
