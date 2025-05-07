d76e2fbd6548f238a2aae6d549644cff559b8882
Dylan Bowman <dylanbowmandsk@gmail.com>
2025-05-07 09:38:39 +0100

    made changes to contact mail handling

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,14 +1,23 @@
 "use server"
 
 import { Resend} from "resend"
-
-const resend = new Resend(process.env.RESEND_API_KEY)
+const apiKey = process.env.RESEND_API_KEY
+const resend = new Resend(apiKey)
 
 export const sendEmail = async (name: string,email: string,message: string) => {
+  try{
     resend.emails.send({
         from: "onboarding@resend.dev",
         to: "dylanbowmandsk@gmail.com",
         subject: "New mail from portfolio website",
         html: `<p> ${name}</p><p>${email}</p><p>${message}</p>`
       });
+    }
+    catch (error: unknown) {
+      if (error instanceof Error) {
+        return { error: error.message };
+      } else {
+        return { error: "An unknown error occurred" };
+      }
+  }
 }
