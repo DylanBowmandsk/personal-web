95dcb5890bd7074eb7af36a158c3b1744a00d342
Dylan Bowman <dylanbowmandsk@gmail.com>
2025-05-07 10:29:51 +0100

    changed input to text box

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -16,9 +16,9 @@ const Contact = () => {
             <h1 className="text-4xl text-violet-400 font-light poppins-light mb-10" data-aos="fade-right">{"Contact me"}</h1>
             <form action={send}>
                 <div className="flex flex-col">
-                    <input type="text" name="name" id="name" onChange={e => setName(e.target.value)} placeholder="name" className="border-b-1 h-14 w-100 md:w-140 text-xl border-solid mb-4"/>
-                    <input type="text" name="mail" id="email" onChange={e => setEmail(e.target.value)} placeholder="email" className="border-b-1 w-100 md:w-140 h-14 text-xl border-solid mb-4"/>
-                    <input type="text" name="message" id="message" onChange={e => setMessage(e.target.value)} placeholder="your message" className="border-b-1 h-14 w-100 md:w-140 text-xl border-solid mb-4"/>
+                    <input type="text" name="name" id="name" onChange={e => setName(e.target.value)} placeholder="name" className="border-b-1 h-14 w-80 md:w-140 text-xl border-solid mb-4"/>
+                    <input type="text" name="mail" id="email" onChange={e => setEmail(e.target.value)} placeholder="email" className="border-b-1 h-14 w-80 md:w-140 text-xl border-solid mb-4"/>
+                    <textarea name="message" id="message" onChange={e => setMessage(e.target.value)} placeholder="your message" className="border-b-1 h-34 w-80 md:w-140 text-xl border-solid mb-4"/>
                 </div>
                 <button type="submit" className="rounded-full bg-violet-400 hover:bg-violet-500 text-white poppins-semibold py-2 px-4 rounded-lg shadow-md transition-colors duration-200">Send</button>
             </form>
