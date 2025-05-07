8a77c8bf2aff7390e1c6f9f634a91dd14bfad9cb
Dylan Bowman <dylanbowmandsk@gmail.com>
2025-05-07 10:42:42 +0100

    changed viewport handling with fading animations

--- src/app/work.tsx
+++ src/app/work.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
-
+import { useEffect, useState } from "react";
 const Work = () => {
+
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
   return (
     <div id="work" className="flex flex-col items-center justify-center px-4 md:px-20 lg:px-40 bg-stone-950 text-zinc-100">
       <h1 className="py-20 text-4xl poppins-light text-violet-400" data-aos="fade-right">{"Work"}</h1>
 
       <div className="flex flex-col md:flex-row w-full max-w-7xl gap-46 items-center mb-24">
-        <div className="flex-1 text-zinc-100 font-light" data-aos="fade-right">
+        <div className="flex-1 text-zinc-100 font-light" data-aos={width < 700 ?"":"fade-right"}>
           <h2 className="text-3xl poppins-semibold mb-2 text-violet-400">Self-Employed</h2>
           <div className="mb-4 border-b border-zinc-700">
             <h3 className="inline text-lg font-medium text-xl">Web3 Developer</h3>
@@ -20,7 +27,7 @@ const Work = () => {
           </p>
         </div>
 
-        <div className="flex-1 flex justify-end items-center" data-aos="fade-left">
+        <div className="flex-1 flex justify-end items-center" data-aos={width < 700 ?"":"fade-left"}>
           <div className="w-[480px] h-[280px] overflow-hidden rounded-xl shadow-lg">
             <Image src="/blockchain-image.PNG" width={800} height={800} alt="Web3 Project Visual" className="object-cover w-full h-full"
             />
@@ -29,14 +36,14 @@ const Work = () => {
       </div>
 
       <div className="flex flex-col-reverse md:flex-row w-full max-w-7xl gap-46 items-center mb-24">
-        <div className="flex-1 flex justify-start items-center" data-aos="fade-right">
+        <div className="flex-1 flex justify-start items-center" data-aos={width < 700 ?"":"fade-right"}>
           <div className="w-[480px] h-[280px] overflow-hidden rounded-xl shadow-lg">
             <Image src="/genius-image.png" width={800} height={800} alt="Genius SSLC" className="object-cover w-full h-full"
             />
           </div>
         </div>
 
-        <div className="flex-1 text-zinc-100 font-light"data-aos="fade-left">
+        <div className="flex-1 text-zinc-100 font-light" data-aos={width < 700 ?"":"fade-left"}>
           <h2 className="text-3xl poppins-semibold mb-2 text-violet-400">Genius SSLC</h2>
           <div className="mb-4 border-b border-zinc-700">
             <h3 className="inline text-lg font-medium text-xl">Software Engineer</h3>
@@ -52,7 +59,7 @@ const Work = () => {
       </div>
 
       <div className="flex flex-col md:flex-row w-full max-w-7xl gap-46 items-center mb-24">
-        <div className="flex-1 text-zinc-100 font-light" data-aos="fade-right">
+        <div className="flex-1 text-zinc-100 font-light" data-aos={width < 700 ?"":"fade-right"}>
           <h2 className="text-3xl poppins-semibold mb-2 text-violet-400">Peter Vardy</h2>
           <div className="mb-4 border-b border-zinc-700">
             <h3 className="inline text-lg font-medium text-xl">Application Developer</h3>
@@ -65,7 +72,7 @@ const Work = () => {
           </p>
         </div>
 
-        <div className="flex-1 flex justify-end items-center" data-aos="fade-left">
+        <div className="flex-1 flex justify-end items-center" data-aos={width < 700 ?"":"fade-left"}>
           <div className="w-[480px] h-[280px] overflow-hidden rounded-xl shadow-lg">
             <Image src="/pv-image.PNG" width={800} height={800} alt="Peter Vardy Office" className="object-cover w-full h-full"
             />
